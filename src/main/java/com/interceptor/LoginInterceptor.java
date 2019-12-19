2a48985a2a591c677760d3dbb6d4a9f58074ace6
taylor <pangjian78@gmail.com>
2019-12-19 09:59:27 +0800

    ssm框架

---
+++ src/main/java/com/interceptor/LoginInterceptor.java
@@ -0,0 +1,34 @@
+package com.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    /**
+     * 负责权限验证，防止用户直接输入url访问
+     * @param httpServletRequest
+     * @param httpServletResponse
+     * @param o
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+
+        //初始默认全部放行
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
---
+++ src/main/java/com/main/GeneraterSqlMap.java
@@ -0,0 +1,36 @@
+package com.main;
+
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GeneraterSqlMap {
+    public void generator() throws Exception {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        // 指定配置文件
+        File configFile = new File("D:\\file\\webapp\\teacher_manager_system\\src\\main\\resources\\generatorConfig.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+    // 执行main方法以生成代码
+    public static void main(String[] args) {
+        try {
+            GeneraterSqlMap generatorSqlmap = new GeneraterSqlMap();
+            generatorSqlmap.generator();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
